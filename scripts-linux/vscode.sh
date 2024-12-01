783596d0ee530df242d0ad1d927b7b9ed72a8015
Theo <130441797+theguega@users.noreply.github.com>
2024-12-01 18:07:47 +0100

    move vscode extensions installation

--- scripts-linux/vscode.sh
+++ scripts-linux/vscode.sh
@@ -6,6 +6,17 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 . $SCRIPT_DIR/utils.sh
 
 configure_vscode() {
+    info "Installing VSCode extensions"
+    code --install-extension mhutchie.git-graph
+    code --install-extension "charliermarsh.ruff"
+    code --install-extension "thang-nm.catppuccin-perfect-icons"
+    code --install-extension "Catppuccin.catppuccin-vsc"
+    code --install-extension "oderwat.indent-rainbow"
+    code --install-extension "evondev.indent-rainbow-palettes"
+    code --install-extension "be5invis.vscode-custom-css"
+    code --install-extension "supermaven.supermaven"
+    info "VSCode extensions installed successfully."
+
     info "Giving vscode admin priviledge for Custom CSS - JS"
     sudo chown -R $(whoami) "$(which code)"
     sudo chown -R $(whoami) /usr/share/code
