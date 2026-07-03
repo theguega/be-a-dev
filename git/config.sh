b68c7e5d3ededd94ffc590ad9bb925110bf84ab5
Theo <theo@opalin.ai>
2026-07-03 10:32:01 -0700

    update git config

--- git/config.sh
+++
@@ -1,3 +0,0 @@
-git config --global alias.wta '!f() { git worktree add -b "$1" "../$1"; }; f'
-git config --global alias.wtr '!f() { git worktree remove "../$1"; }; f'
-git config --global alias.wtl '!f() { git worktree list; }; f'
